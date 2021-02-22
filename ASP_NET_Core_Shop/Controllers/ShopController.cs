307dd806bdee3955c3658d01125277270cc06baa
Ray <jackboy1006@gmail.com>
2021-02-22 23:30:09 +0800

    Set HomePage Action and view

--- ASP_NET_Core_Shop/Controllers/ShopController.cs
+++ ASP_NET_Core_Shop/Controllers/ShopController.cs
@@ -3,14 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ASP_NET_Core_Shop.Models;
 
 namespace ASP_NET_Core_Shop.Controllers
 {
 	public class ShopController : Controller
 	{
-		public IActionResult Index()
-		{
+		//連接資料庫
+		private readonly ShopDBContext _db;
+		public ShopController(ShopDBContext context)
+        {
+			_db = context;
+        }
+
+
+		public IActionResult HomePage()
+        {
 			return View();
-		}
+        }
+
 	}
 }
